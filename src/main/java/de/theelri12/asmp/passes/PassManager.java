b4fb369f4c5bcabb75802a16ba634a941efb973c
TheElri12 <177674346+TheElri12@users.noreply.github.com>
2026-09-22 15:57:39 +0200

    Fixed a PDC bug

--- src/main/java/de/theelri12/asmp/passes/PassManager.java
+++ src/main/java/de/theelri12/asmp/passes/PassManager.java
@@ -1,10 +1,13 @@
 package de.theelri12.asmp.passes;
 
-import de.theelri12.asmp.pdc.PDCKeys;
+import de.theelri12.asmp.main.Main;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PassManager {
 
@@ -13,15 +16,15 @@ public class PassManager {
     public static int getPassLevel(Player player, String pass) {
         pdc = player.getPersistentDataContainer();
 
-        NamespacedKey key = new NamespacedKey("asmp", pass + "_pass_claimed_tiers");
+        NamespacedKey key = new NamespacedKey(Main.getPlugin(), pass + "_pass_pass_level");
         return pdc.getOrDefault(key, PersistentDataType.INTEGER, 0);
     }
 
-    public static int getClaimedTiers(Player player, String pass) {
+    public static List<Integer> getClaimedTiers(Player player, String pass) {
         pdc = player.getPersistentDataContainer();
 
-        NamespacedKey key = new NamespacedKey("asmp", pass + "_pass_claimed_tiers");
-        return pdc.getOrDefault(key, PersistentDataType.INTEGER, 0);
+        NamespacedKey key = new NamespacedKey(Main.getPlugin(), pass + "_pass_claimed_tiers");
+        return pdc.getOrDefault(key, PersistentDataType.LIST.integers(), new ArrayList<>());
     }
 
 }
