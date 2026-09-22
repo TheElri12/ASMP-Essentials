452b33fae8ea9f6316ff1d16308aa4822bcbee48
TheElri12 <177674346+TheElri12@users.noreply.github.com>
2026-09-22 15:48:10 +0200

    Removed Unlockable and finished the DataTypes

--- src/main/java/de/theelri12/asmp/passes/PassInventories.java
+++ src/main/java/de/theelri12/asmp/passes/PassInventories.java
@@ -1,5 +1,6 @@
 package de.theelri12.asmp.passes;
 
+import de.theelri12.asmp.hooks.CraftEngineHook;
 import de.theelri12.asmp.main.Main;
 import de.theelri12.asmp.passes.data_types.TierReward;
 import de.theelri12.asmp.passes.data_types.TierRewardPDC;
@@ -11,6 +12,8 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.*;
@@ -18,9 +21,14 @@ import java.util.*;
 public class PassInventories {
 
     private static ItemStack setPDC(ItemStack tierItem, int cost, int level, TierReward reward) {
-        tierItem.getItemMeta().getPersistentDataContainer().set(PDCKeys.ITEM_COST, PersistentDataType.INTEGER, cost);
-        tierItem.getItemMeta().getPersistentDataContainer().set(PDCKeys.ITEM_LEVEL, PersistentDataType.INTEGER, level);
-        tierItem.getItemMeta().getPersistentDataContainer().set(PDCKeys.TIER_REWARD, new TierRewardPDC(Main.getPlugin()), reward);
+        ItemMeta meta = tierItem.getItemMeta();
+        PersistentDataContainer pdc = meta.getPersistentDataContainer();
+
+        pdc.set(PDCKeys.ITEM_COST, PersistentDataType.INTEGER, cost);
+        pdc.set(PDCKeys.ITEM_LEVEL, PersistentDataType.INTEGER, level);
+        pdc.set(PDCKeys.TIER_REWARD, new TierRewardPDC(Main.getPlugin()), reward);
+
+        tierItem.setItemMeta(meta);
         return tierItem;
     }
 
@@ -38,61 +46,61 @@ public class PassInventories {
         List<ItemStack> tiersPage4 = new ArrayList<>();
 
         //Tier Items Seite 1
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 1, TierReward.item()));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 2));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 3));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 4));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 5));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 6));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 7));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 8));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 9));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 10));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 11));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 12));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 13));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 14));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 15));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 16));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 17));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 18));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 19));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 20));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 21));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 22));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 23));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 24));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 25));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 26));
-        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 27));
+        tiersPage1.add(setPDC(CraftEngineHook.getCraftEngineItem("asmp:spruce_dark_oak_table"), 5, 1, TierReward.unlockable("asmp:spruce_dark_oak_table", 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 2, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 3, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 4, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 5, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 6, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 7, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 8, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 9, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 10, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 11, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 12, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 13, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 14, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 15, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 16, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 17, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 18, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 19, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 20, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 21, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 22, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 23, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 24, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 25, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 26, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage1.add(setPDC(new ItemStack(Material.DIAMOND), 5, 27, TierReward.item(Material.DIAMOND, 1)));
 
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 1 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 2 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 3 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 4 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 5 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 6 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 7 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 8 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 9 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 10 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 11 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 12 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 13 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 14 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 15 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 16 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 17 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 18 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 19 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 20 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 21 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 22 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 23 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 24 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 25 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 26 +27));
-        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 27 +27));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 1 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 2 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 3 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 4 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 5 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 6 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 7 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 8 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 9 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 10 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 11 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 12 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 13 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 14 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 15 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 16 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 17 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 18 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 19 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 20 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 21 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 22 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 23 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 24 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 25 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 26 +27, TierReward.item(Material.DIAMOND, 1)));
+        tiersPage2.add(setPDC(new ItemStack(Material.DIAMOND), 5, 27 +27, TierReward.item(Material.DIAMOND, 1)));
         //Tier Items
         pages.put(1, tiersPage1);
         pages.put(2, tiersPage2);
